8497e6afad87c061400933e07d58043f3836c7c6
Nathan Collier <nathaniel.collier@gmail.com>
2024-02-02 11:14:23 -0500

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # solr2globus
+
+This utility script is used to query a Solr-based ESGF index, harvest the metadata information, and then submit it to a Globus-based index. This utility does not need to be installed. You will just need to install the dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Notes
+
+- You must have `owner`, `admin`, or `writer` access to the `globus_index_id` to which you wish to ingest. 
+- The utility will use the `backoff` library to continue to make the required requests to both indices until they are successful, gradually increasing the time between requests.
+- As we anticipate running this utility relatively few times, you will need to authenticate for each execution. If annoying, we could add refresh token support.
+- You may ingest by searches using `esg-search` syntax. A sample query is provided which took ~30 seconds to submit the ingest request.
+- This utility does not monitor when the Globus ingest tasks have completed--we just ensure that they were accepted. There can be a time lag between when you make the request and when your entries will appear in the new index.
---
+++ solr2globus.py
@@ -0,0 +1,121 @@
+import datetime
+import logging
+import time
+
+import backoff
+import requests
+from globus_sdk import (
+    AccessTokenAuthorizer,
+    GlobusError,
+    NativeAppAuthClient,
+    SearchClient,
+)
+from tqdm import tqdm
+
+
+def get_authorization():
+    """Get an authorizer that will allow the ingest.
+
+    You will need to be the owner/admin of the globus index into which you wish to
+    ingest. The client uuid I have hard-coded in here is one I registered under the
+    ESGF2 project for solr2globus. As far as I know, this just gives Globus a
+    responsible application when looking in logs.
+    """
+    client = NativeAppAuthClient("cb4f0bba-e44c-4a36-9023-06929dbb4742")
+    client.oauth2_start_flow(
+        requested_scopes=["urn:globus:auth:scope:search.api.globus.org:all"],
+        refresh_tokens=False,
+    )
+    authorize_url = client.oauth2_get_authorize_url()
+    print(
+        f"""
+All interactions with Globus must be authorized. Go here:
+
+{authorize_url}
+
+and paste the link here:\n"""
+    )
+    auth_code = input("> ").strip()
+    token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+    auth = AccessTokenAuthorizer(token_response["access_token"])
+    return auth
+
+
+@backoff.on_exception(backoff.expo, requests.exceptions.RequestException)
+def esg_search(base_url, **search):
+    """Return an esg-search response as a dictionary."""
+    if "format" not in search:
+        search["format"] = "application/solr+json"
+    response = requests.get(f"{base_url}/esg-search/search", params=search)
+    response.raise_for_status()
+    return response.json()
+
+
+@backoff.on_exception(backoff.expo, (requests.exceptions.RequestException, GlobusError))
+def ingest(client, entries):
+    response = client.ingest(
+        globus_index_id,
+        {
+            "ingest_type": "GMetaList",
+            "ingest_data": {"gmeta": entries},
+        },
+    )
+    if not (response.data["acknowledged"] and response.data["success"]):
+        logger.info(f"{response.data}")
+        raise GlobusError()
+
+
+def ingest_by_search(client, chunk_size=1000, **search):
+    """Ingest the records found in the given search, `chunk_size` at a time."""
+    ingest_time = time.time()
+    # To know how many chunks to submits, we need to know how many results
+    num_results = esg_search(solr_base_url, limit=0, **search)["response"]["numFound"]
+    num_chunks = int(num_results / chunk_size) + 1
+    for i in tqdm(range(num_chunks), unit="chunk"):
+        data = esg_search(
+            solr_base_url, offset=i * chunk_size, limit=chunk_size, **search
+        )
+        entries = []
+        for doc in data["response"]["docs"]:
+            gmeta_entry = {
+                "id": search["type"].lower() if "type" in search else "dataset",
+                "subject": doc["id"],
+                "visible_to": ["public"],
+                "content": doc,
+            }
+            entries.append(gmeta_entry)
+        ingest(client, entries)
+    ingest_time = time.time() - ingest_time
+    logger.info(
+        f"ingested {num_results} records in {ingest_time:.2f} [s] at {num_results/ingest_time:.2f} record/s"
+    )
+
+
+if __name__ == "__main__":
+    # Configure the source Solr index by URL and the target Globus index by UUID
+    solr_base_url = "http://esgf-node.ornl.gov"
+    globus_index_id = "ea4595f4-7b71-4da7-a1f0-e3f5d8f7f062"
+
+    # Setup some logging, you will see Globus' logs too
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s: %(message)s",
+        filename="ingest.log",
+    )
+    logger = logging.getLogger(__name__)
+
+    # Create a client authorized to ingest and then ingest by a search. Files and
+    # Datasets must be ingested separately as esg-search does not query both types
+    # simultaneously.
+    client = SearchClient(authorizer=get_authorization())
+    ingest_time = time.time()
+    for entry_type in ["Dataset", "File"]:
+        ingest_by_search(
+            client,
+            experiment_id="historical",
+            source_id="CESM2",
+            variant_label="r1i1p1f1",
+            type=entry_type,
+        )
+    ingest_time = time.time() - ingest_time
+    logger.info(f"ingest total time {str(datetime.timedelta(seconds=ingest_time))}")
