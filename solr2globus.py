8929431149ec5e5c3ec5fd13170ecaacfb9672ff
Nathan Collier <nathaniel.collier@gmail.com>
2024-03-13 07:50:25 -0400

    refactor with instructions for future users

--- solr2globus.py
+++ solr2globus.py
@@ -26,15 +26,11 @@ def get_authorized_search_client() -> sdk.SearchClient:
         authorize_url = client.oauth2_get_authorize_url()
         print(
             f"""
-All interactions with Globus must be authorized. To ensure that we have permission to faci
-liate your transfer, please open the following link in your browser.
+All interactions with Globus must be authorized. To ensure that we have permission to faciliate your transfer, please open the following link in your browser.
 
 {authorize_url}
 
-You will have to login (or be logged in) to your Globus account. Globus will also request 
-that you give a label for this authorization. You may pick anything of your choosing. Afte
-r following the instructions in your browser, Globus will generate a code which you must c
-opy and paste here and then hit <enter>.\n"""
+You will have to login (or be logged in) to your Globus account. Globus will also request that you give a label for this authorization. You may pick anything of your choosing. After following the instructions in your browser, Globus will generate a code which you must copy and paste here and then hit <enter>.\n"""
         )
         auth_code = input("> ").strip()
         token_response = client.oauth2_exchange_code_for_tokens(auth_code)
@@ -101,6 +97,9 @@ def ingest_by_search(client, chunk_size=1000, **search):
         data = esg_search(
             solr_base_url, offset=i * chunk_size, limit=chunk_size, **search
         )
+        # We may perfectly page to the last entry and thus get a 0-length docs
+        if data["response"]["numFound"] == data["response"]["start"]:
+            continue
         entries = []
         for doc in data["response"]["docs"]:
             # doc = amend_doc(doc)
@@ -126,7 +125,6 @@ if __name__ == "__main__":
     # Configure the source Solr index by URL and the target Globus index by UUID
     solr_base_url = "http://esgf-node.ornl.gov"
     globus_index_id = "ea4595f4-7b71-4da7-a1f0-e3f5d8f7f062"
-    project = "e3sm"
 
     # Setup some logging, you will see Globus' logs too
     logging.basicConfig(
@@ -136,33 +134,49 @@ if __name__ == "__main__":
     )
     logger = logging.getLogger(__name__)
 
-    # Authorize our client
+    # We recommend that you ingest records by projects and across a set of facets. This
+    # is because we observe that when there are too many pages of results, the
+    # performance of the Solr-index via esg_search degrades substantially.
+    PROJECT = "CMIP6"
+    BY_FACET = "experiment_id"
+    RECORD_TYPE = "File"  # or Dataset
+
+    # The larger this is, the faster the ingest will be. However, globus has a limit on
+    # the memory that can be ingested at a time. Since different projects store
+    # different data in their records, you may have to reduce this if you notice in the
+    # log that your request is being rejected.
+    CHUNK_SIZE = 1000
+
+    # Authorize our client to ingest
     client = get_authorized_search_client()
 
-    # Ingests will take place on a per-experiment basis
+    # Get the facets for the project we are ingesting
     response = esg_search(
         "http://esgf-node.ornl.gov",
-        type="File",
+        type=RECORD_TYPE,
+        project=PROJECT,
+        facets=BY_FACET,
         limit=0,
-        project=project,
-        experiment=[
-            # "hist-aer",
-            "hist-nat",
-            "amip",
-            "hist-GHG",
-            "piControl",
-            "historical",
-        ],
-        facets="experiment",
     )
-    experiments = response["facet_counts"]["facet_fields"]["experiment"][::2]
-    counts = response["facet_counts"]["facet_fields"]["experiment"][1::2]
-    experiments = [e for _, e in sorted(zip(counts, experiments))]
+    facets = [
+        e
+        for _, e in sorted(
+            zip(
+                response["facet_counts"]["facet_fields"][BY_FACET][1::2],
+                response["facet_counts"]["facet_fields"][BY_FACET][::2],
+            )
+        )
+    ]
 
-    for experiment in experiments:
+    # Main work loop
+    for facet in facets:
         ingest_time = time.time()
         ingest_by_search(
-            client, type="File", chunk_size=100, project=project, experiment=experiment
+            client,
+            type=RECORD_TYPE,
+            chunk_size=CHUNK_SIZE,
+            project=PROJECT,
+            **{BY_FACET: facet},
         )
         ingest_time = time.time() - ingest_time
         logger.info(f"ingest total time {str(datetime.timedelta(seconds=ingest_time))}")
